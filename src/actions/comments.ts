fdcd66c6aad8cee79800e89223f6a87504a3bb0b
Matias Calvar <matias.calvar@ortex.com>
2024-06-09 23:18:20 +0100

    add: create comment controller

--- src/actions/comments.ts
+++ src/actions/comments.ts
@@ -1,6 +1,34 @@
 import axios from "axios";
 import { Comment } from "@/types/interfaces";
 
+export const createComment = async (
+  postId: number,
+  name: string,
+  email: string,
+  body: string
+) => {
+  try {
+    const res = await axios.post<Comment>(
+      `https://jsonplaceholder.typicode.com/comments/`,
+      {
+        postId,
+        name,
+        email,
+        body,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json; charset=UTF-8",
+        },
+      }
+    );
+
+    return res.data;
+  } catch (err) {
+    throw err;
+  }
+};
+
 export const updateComment = async (commentId: number, body: string) => {
   try {
     const res = await axios.patch<Comment>(
