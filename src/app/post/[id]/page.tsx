48ebeff2f7d924959e8f8d89d070d79984c565e6
Matias Calvar <matias.calvar@ortex.com>
2024-06-10 17:50:40 +0100

    add: create new comment logic

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -1,10 +1,14 @@
 "use client";
-import { deleteComment, updateComment } from "@/actions/comments";
+import {
+  createComment,
+  deleteComment,
+  updateComment,
+} from "@/actions/comments";
 import { getComments } from "@/actions/posts";
 import { useParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 
-const Post = () => {
+const PostDetail = () => {
   const { id } = useParams();
 
   const [comments, setComments] = useState<
@@ -13,6 +17,22 @@ const Post = () => {
   const [loading, setLoading] = useState(true);
   const [editingCommentId, setEditingCommentId] = useState<number | null>(null);
   const [editedText, setEditedText] = useState("");
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    newComment: "",
+  });
+
+  const handleFormChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
 
   const handleEditing = (commentId: number, commentBody: string) => {
     setEditingCommentId(commentId);
@@ -37,6 +57,25 @@ const Post = () => {
     }
   };
 
+  const handleAddition = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const { name, email, newComment } = formData;
+      const resNewComment = await createComment(
+        Number(id),
+        name,
+        email,
+        newComment
+      );
+      setFormData({ name: "", email: "", newComment: "" });
+
+      resNewComment &&
+        setComments((prevComments) => [resNewComment, ...prevComments]);
+    } catch (error) {
+      console.error("Error creating comment:", error);
+    }
+  };
+
   const handleDeletion = async (commentId: number) => {
     try {
       const deletedComment = await deleteComment(commentId);
@@ -50,7 +89,7 @@ const Post = () => {
   };
 
   useEffect(() => {
-    const fetchPosts = async () => {
+    const fetchComments = async () => {
       try {
         const comments = await getComments(id as string);
         setComments(comments);
@@ -61,11 +100,48 @@ const Post = () => {
       }
     };
 
-    fetchPosts();
+    fetchComments();
   }, [id]);
+
   return (
     <div className="h-auto">
       <h1>Dynamic post #{id}</h1>
+      <div className="flex my-5 mx-5">
+        <form onSubmit={handleAddition}>
+          <div>
+            <input
+              type="text"
+              className="w-full my-1"
+              id="name-input"
+              name="name"
+              value={formData.name}
+              onChange={handleFormChange}
+            />
+            <input
+              type="email"
+              className="w-full my-1"
+              id="email-input"
+              name="email"
+              value={formData.email}
+              onChange={handleFormChange}
+            />
+          </div>
+          <textarea
+            value={formData.newComment}
+            name="newComment"
+            onChange={handleFormChange}
+            rows={1}
+            className="w-full p-2 border border-gray-300 ml-1"
+          />
+
+          <button
+            className="mx-5 min-w-[100px] bg-red-500 text-white"
+            type="submit"
+          >
+            Add
+          </button>
+        </form>
+      </div>
       {loading ? (
         <div>Loading...</div>
       ) : comments.length ? (
@@ -121,4 +197,4 @@ const Post = () => {
   );
 };
 
-export default Post;
+export default PostDetail;
