f84101f9580669bb4dfe0745fd53af3516db90b6
Matias Calvar <matias.calvar@ortex.com>
2024-06-11 21:16:08 +0100

    add: user interfaces

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -11,4 +11,41 @@ export interface Post {
   id: string;
   title: string;
   body: string;
+  email?: string;
+  name?: string;
+  initials?: string;
+}
+
+interface Geo {
+  lat: string;
+  lng: string;
+}
+
+interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+}
+
+interface Company {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
+
+export interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: Address;
+  phone: string;
+  website: string;
+  company: Company;
+}
+
+export interface UserLookup {
+  [key: number]: User;
 }
