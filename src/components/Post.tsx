7e58c62fe8b3600e036a9b93f2a2f8778e8cd3e4
Matias Calvar <matias.calvar@ortex.com>
2024-06-11 20:32:34 +0100

    add: Post component

---
+++ src/components/Post.tsx
@@ -0,0 +1,43 @@
+import { Post as PostType } from "@/types/interfaces";
+import Link from "next/link";
+import { FunctionComponent } from "react";
+
+interface PostProps {
+  post: PostType;
+}
+
+const Post: FunctionComponent<PostProps> = ({ post }) => {
+  const colors = [
+    "bg-red-500",
+    "bg-blue-500",
+    "bg-green-500",
+    "bg-yellow-500",
+    "bg-indigo-500",
+    "bg-purple-500",
+    "bg-pink-500",
+  ];
+  const randomColor = () => colors[Math.floor(Math.random() * colors.length)];
+
+  return (
+    <div
+      key={post.id}
+      className="m-2.5 p-2.5 bg-white w-[1000px] rounded-md shadow-md"
+    >
+      <div className="flex items-center">
+        <div
+          className={`flex items-center justify-center rounded-full ${randomColor()} w-8 h-8 mr-2 text-white`}
+        >
+          JD
+        </div>
+        <p className="text-bold">John Doe</p>
+      </div>
+      <p className="font-medium mt-1">{post.title}</p>
+      <p className="text-sm my-1">{post.body}</p>
+      <p className="mt-2 text-xs text-end">
+        <Link href={`/post/${post.id}`}>See more</Link>
+      </p>
+    </div>
+  );
+};
+
+export default Post;
