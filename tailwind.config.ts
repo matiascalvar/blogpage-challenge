4696d9d8f42ae744f1011af45acf0f712e6dc80c
Matias Calvar <matias.calvar@ortex.com>
2024-06-11 20:25:10 +0100

    add: custom colour to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "bi-red": "#cc0000",
+      },
     },
   },
   plugins: [],
