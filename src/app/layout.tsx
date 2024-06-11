45a8f70dfd98666ef38c6d444c455dcac9026b48
Matias Calvar <matias.calvar@ortex.com>
2024-06-11 20:24:40 +0100

    update: layout with new components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
-import { Inter, Montserrat } from "next/font/google";
-import Link from "next/link";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const monserrat = Montserrat({ subsets: ["latin"] });
 
@@ -17,40 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={monserrat.className + " h-[100vh] overflow-hidden"}>
-        <div
-          className="flex justify-between h-72 relative items-start pt-11 shadow-md"
-          style={{
-            backgroundImage: "url(/porto.jpg)",
-            backgroundSize: "cover",
-            backgroundPosition: "top",
-            backgroundRepeat: "no-repeat",
-          }}
-        >
-          {/* <div
-            className="absolute inset-0 bg-gradient-to-b from-transparent to-white/75 to-white"
-            style={{
-              pointerEvents: "none", // Ensures this overlay doesn't interfere with any content
-            }}
-          ></div> */}
-          <div
-            className="min-w-[300px] text-left text-lg text-red-600 text-3xl font-extrabold tracking-widest relative pl-36"
-            id="logo"
-          >
-            <Link href="/">
-              BRIDGE IN<br></br>CHALLENGE
-            </Link>
-          </div>
-          <div className="flex min-w-[300px] justify-between relative pr-36">
-            <div>
-              <Link href="/">Home</Link>
-            </div>
-            <div>
-              <Link href="/about">About</Link>
-            </div>
-          </div>
-        </div>
+      <body className={monserrat.className + " h-[100vh]"}>
+        <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
