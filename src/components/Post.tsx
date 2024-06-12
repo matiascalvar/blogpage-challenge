6f97aaa7878146753299068f047843794a314b27
Matias Calvar <matias.calvar@ortex.com>
2024-06-12 15:59:42 +0100

    improve: post component avatar color

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,31 +1,37 @@
 import { Post as PostType } from "@/types/interfaces";
 import Link from "next/link";
-import { FunctionComponent } from "react";
+import { FunctionComponent, useState } from "react";
 
 interface PostProps {
   post: PostType;
+  mainPage?: boolean;
 }
 
-const Post: FunctionComponent<PostProps> = ({ post }) => {
-  const colors = [
-    "bg-red-500",
-    "bg-blue-500",
-    "bg-green-500",
-    "bg-yellow-500",
-    "bg-indigo-500",
-    "bg-purple-500",
-    "bg-pink-500",
-  ];
-  const randomColor = () => colors[Math.floor(Math.random() * colors.length)];
+const colors = [
+  "bg-red-500",
+  "bg-blue-500",
+  "bg-green-500",
+  "bg-yellow-500",
+  "bg-indigo-500",
+  "bg-purple-500",
+  "bg-pink-500",
+];
+const randomColor = () => colors[Math.floor(Math.random() * colors.length)];
+
+const Post: FunctionComponent<PostProps> = ({ post, mainPage }) => {
+  const [color] = useState(randomColor());
+
   return (
     <div
       key={post.id}
-      className="m-2.5 bg-white w-[1000px] rounded-md shadow-md p-5"
+      className={`m-2.5 bg-white w-[1000px] rounded-md shadow-md p-5 ${
+        mainPage && "hover:shadow-bi-red hover:shadow"
+      }`}
     >
       <div className="flex items-center">
         {post.initials && (
           <div
-            className={`flex items-center justify-center rounded-full ${randomColor()} w-8 h-8 mr-2 text-white`}
+            className={`flex items-center justify-center rounded-full ${color} w-8 h-8 mr-2 text-white`}
           >
             {post.initials}
           </div>
@@ -34,9 +40,11 @@ const Post: FunctionComponent<PostProps> = ({ post }) => {
       </div>
       <p className="font-medium mt-1">{post.title}</p>
       <p className="text-sm my-1">{post.body}</p>
-      <p className="mt-2 text-xs text-end">
-        <Link href={`/post/${post.id}`}>See more</Link>
-      </p>
+      {mainPage && (
+        <p className="mt-2 text-xs text-end">
+          <Link href={`/post/${post.id}`}>See more</Link>
+        </p>
+      )}
     </div>
   );
 };
