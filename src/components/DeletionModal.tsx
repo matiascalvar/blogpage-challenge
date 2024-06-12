25b177a9af90353f5f8f10b5c7ce555835dcfc92
Matias Calvar <matias.calvar@ortex.com>
2024-06-12 16:56:32 +0100

    improve: deletion modal styles

--- src/components/DeletionModal.tsx
+++ src/components/DeletionModal.tsx
@@ -16,30 +16,35 @@ const DeletionModal = ({ handleDeletion, setOpenModal }: Modal) => {
         justifyContent: "center",
         backdropFilter: "blur(1px)",
       }}
+      onClick={() => setOpenModal(false)}
     >
       <div
         style={{
-          width: "12rem",
-          height: "5rem",
           maxWidth: "100vw",
           maxHeight: "100dvh",
           backgroundColor: "white",
           padding: "1rem",
           borderRadius: "0.5rem",
           boxShadow: "0 2px 10px rgba(0, 0, 0, 0.1)",
+          textAlign: "center",
         }}
       >
-        Delete comment?
+        <p className="my-2">Delete comment?</p>
         <div>
           <button
-            style={{ backgroundColor: "red" }}
+            className="min-w-[100px] bg-red-500 text-white rounded-md text-sm min-h-6 mx-2"
             onClick={() => {
               handleDeletion(), setOpenModal(false);
             }}
           >
             Delete
           </button>
-          <button onClick={() => setOpenModal(false)}>Cancel</button>
+          <button
+            className="min-w-[100px] border rounded-md text-sm min-h-6 mx-2"
+            onClick={() => setOpenModal(false)}
+          >
+            Cancel
+          </button>
         </div>
       </div>
     </div>
