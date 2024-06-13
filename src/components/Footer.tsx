250bd8367835150bd96d098e9fdaf9feae5bf1d2
Matias Calvar <matias.calvar@ortex.com>
2024-06-13 18:30:09 +0100

    update: footer position to bottom

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { DiReact } from "react-icons/di";
 
 const Footer = () => {
   return (
-    <div className="flex justify-end items-center bg-bi-red fixed bottom-0 h-[25px] text-white shadow text-xs pr-5 w-screen">
+    <div className="flex justify-end items-center bg-bi-red  bottom-0 h-[25px] text-white shadow text-xs pr-5 w-screen">
       <Link href={"https://www.linkedin.com/in/matiascalvar/"}>
         <p className="font-medium tracking-wide flex">
           with
