a5ffceb12ac14fb9b2ad334eb35302071396b754
Matias Calvar <matias.calvar@ortex.com>
2024-06-13 18:45:31 +0100

    improve: not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,19 +1,15 @@
 import Link from "next/link";
 import Image from "next/image";
+import Button from "@/components/button/Buttton";
 
 const NotFound = () => {
   return (
-    <div className="flex flex-col justify-center items-center p-10">
+    <div className="flex flex-col justify-center items-center p-10 h-[60vh]">
       <Image src="/404.svg" alt="404-image" width={200} height={200} />
       <h1 className="mt-5">Nothing to see here.</h1>
-      <h2 className="my-2">
-        Go back to
-        <Link href="/">
-          <button className="mx-3 px-5 border rounded-md text-sm min-h-6 hover:border-bi-red">
-            Home
-          </button>
-        </Link>
-      </h2>
+      <Link href="/" className="my-4">
+        <Button type="" text="Home" />
+      </Link>
     </div>
   );
 };
