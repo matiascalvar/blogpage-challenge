c393f4ce2a80c389e405f5f3c9675cc2dfa41320
Matias Calvar <matias.calvar@ortex.com>
2024-06-13 19:43:02 +0100

    fix: button color for each type

--- src/components/button/Buttton.tsx
+++ src/components/button/Buttton.tsx
@@ -8,16 +8,16 @@ interface ButtonProps {
 const Button = ({ type, text, onClick, isSubmit = false }: ButtonProps) => {
   const getButtonType = (type: string) => {
     if (type === "warning") {
-      return "bg-red-500 hover:bg-red-800";
+      return "bg-red-500 hover:bg-red-800 text-white";
     } else if (type === "confirm") {
-      return "bg-green-500 hover:bg-green-800";
+      return "bg-green-500 hover:bg-green-800 text-white";
     } else {
       return "border rounded-md text-black hover:border-gray-800";
     }
   };
   return (
     <button
-      className={`min-w-[120px] text-white rounded-md text-sm min-h-6 ease-in duration-300 ${getButtonType(
+      className={`min-w-[120px] rounded-md text-sm min-h-6 ease-in duration-300 ${getButtonType(
         type
       )}`}
       type={isSubmit ? "submit" : "button"}
