28154c7d2c84d3f36e97f3719a26587394b444ea
Matias Calvar <matias.calvar@ortex.com>
2024-06-13 11:53:26 +0100

    improve: footer styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,10 +3,14 @@ import { DiReact } from "react-icons/di";
 
 const Footer = () => {
   return (
-    <div className="flex justify-end items-center w-[100%] bg-bi-red fixed bottom-0 h-[25px] text-white shadow text-xs pr-5">
+    <div className="flex justify-end items-center bg-bi-red fixed bottom-0 h-[25px] text-white shadow text-xs pr-5 w-screen">
       <Link href={"https://www.linkedin.com/in/matiascalvar/"}>
         <p className="font-medium tracking-wide flex">
-          Made with <DiReact size={"1.5em"} /> by Matias Calvar
+          with
+          <span className="mx-0.5">
+            <DiReact size={"1.5em"} />
+          </span>
+          by Matias Calvar
         </p>
       </Link>
     </div>
