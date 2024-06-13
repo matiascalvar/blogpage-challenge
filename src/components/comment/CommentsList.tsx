523b198333e70fdcacf3dc804daa95e861ff0115
Matias Calvar <matias.calvar@ortex.com>
2024-06-13 14:24:32 +0100

    add: CommentsList component

---
+++ src/components/comment/CommentsList.tsx
@@ -0,0 +1,36 @@
+import { FC } from "react";
+import Comment from "./Comment";
+import { Comment as CommentType } from "@/types/interfaces";
+
+interface CommentsListProps {
+  comments: CommentType[];
+  handleEditing: (commentId: number | null) => void;
+  editingCommentId: number | null;
+  handleSave: (commentId: number, editedText: string) => Promise<void>;
+  handleModal: any;
+}
+
+const CommentsList: FC<CommentsListProps> = ({
+  comments,
+  handleEditing,
+  editingCommentId,
+  handleSave,
+  handleModal,
+}) => {
+  return (
+    <div>
+      {comments.map((comment) => (
+        <Comment
+          key={comment.id}
+          comment={comment}
+          handleEditing={handleEditing}
+          editingCommentId={editingCommentId}
+          handleSave={handleSave}
+          handleModal={handleModal}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default CommentsList;
