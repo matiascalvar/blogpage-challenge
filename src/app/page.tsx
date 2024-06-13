f6cb0491a832433c51d40f2fa89213df0280dae3
Matias Calvar <matias.calvar@ortex.com>
2024-06-13 13:14:41 +0100

    add: Pagination to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,15 @@
 
 import Loading from "@/components/Loading";
 import Post from "@/components/Post";
+import Pagination from "@/components/pagination/Pagination";
+import usePagination from "@/hooks/usePagination";
 import usePosts from "@/hooks/usePosts";
 
 const App = () => {
   const { isLoading, data: allPosts, error } = usePosts();
+
+  const { currentPage, itemsPerPage, currentItems, handlePagination } =
+    usePagination(allPosts, 5);
 
   if (isLoading) return <Loading />;
   else if (error) return <div>There was an error. Try again later.</div>;
@@ -17,9 +22,15 @@ const App = () => {
           className="flex flex-col items-center"
           style={{ position: "relative", top: "-30px" }}
         >
-          {allPosts.map((post) => (
+          {currentItems.map((post) => (
             <Post post={post} key={post.id} mainPage />
           ))}
+          <Pagination
+            length={allPosts.length}
+            postsPerPage={itemsPerPage}
+            handlePagination={handlePagination}
+            currentPage={currentPage}
+          />
         </div>
       ) : (
         <div>No posts available.</div>
