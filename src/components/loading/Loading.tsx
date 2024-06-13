d1c18b271f818d5bdae5b3cde0dfc43f71ce98f1
Matias Calvar <matias.calvar@ortex.com>
2024-06-13 18:29:51 +0100

    update: loading height using variables

--- src/components/loading/Loading.tsx
+++ src/components/loading/Loading.tsx
@@ -2,7 +2,9 @@ import styles from "./Loading.module.css";
 
 const Loading = () => {
   return (
-    <div className="absolute inset-x-1/2 inset-y-1/2">
+    <div
+      className={styles.Loading + " flex justify-center items-center w-[100vw]"}
+    >
       <div className={styles["lds-ellipsis"]}>
         <div></div>
         <div></div>
