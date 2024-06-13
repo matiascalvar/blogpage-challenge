eec56f275ed8dbec6c14693c6153f942dbcaea8e
Matias Calvar <matias.calvar@ortex.com>
2024-06-13 14:25:03 +0100

    update: post page with new components

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -1,19 +1,20 @@
 "use client";
-import { ChangeEvent, FormEvent, useState } from "react";
+import { Dispatch, FormEvent, SetStateAction, useState } from "react";
 import { useParams } from "next/navigation";
 import DeletionModal from "@/components/DeletionModal";
+import Loading from "@/components/Loading";
+import Post from "@/components/Post";
+import Pagination from "@/components/pagination/Pagination";
+import Form from "@/components/form/Form";
+import CommentsList from "@/components/comment/CommentsList";
+import usePostAndComments from "@/hooks/usePostAndComments";
+import usePagination from "@/hooks/usePagination";
 import {
   createComment,
   deleteComment,
   updateComment,
 } from "@/actions/comments";
-import usePostAndComments from "@/hooks/usePostAndComments";
-import Loading from "@/components/Loading";
-import Post from "@/components/Post";
-import { RiEdit2Line } from "react-icons/ri";
-import { RiDeleteBin2Line } from "react-icons/ri";
-import usePagination from "@/hooks/usePagination";
-import Pagination from "@/components/pagination/Pagination";
+import { FormData } from "@/types/interfaces";
 
 const PostDetail = () => {
   const { id } = useParams();
@@ -23,41 +24,18 @@ const PostDetail = () => {
   const { currentPage, itemsPerPage, currentItems, handlePagination } =
     usePagination(comments, 3);
 
-  console.log({ currentItems, currentPage, itemsPerPage });
-
   const [editingCommentId, setEditingCommentId] = useState<number | null>(null);
-  const [editedText, setEditedText] = useState("");
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    newComment: "",
-  });
-  const [currentCommentId, setCurrentCommentId] = useState<number>();
   const [openModal, setOpenModal] = useState(false);
+  const [currentCommentId, setCurrentCommentId] = useState<number>();
 
-  const handleFormChange = (
-    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
-    const { name, value } = e.target;
-
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-  };
-
-  const handleEditing = (commentId: number, commentBody: string) => {
+  const handleEditing = (commentId: number | null) => {
     setEditingCommentId(commentId);
-    setEditedText(commentBody);
   };
 
-  const handleSave = async (commentId: number) => {
+  const handleSave = async (commentId: number, editedText: string) => {
     try {
       const updatedComment = await updateComment(commentId, editedText);
-
       setEditingCommentId(null);
-      setEditedText("");
-
       const updateComments = comments.map((comment) =>
         commentId === comment.id
           ? { ...comment, body: updatedComment.body }
@@ -69,7 +47,11 @@ const PostDetail = () => {
     }
   };
 
-  const handleAddition = async (e: FormEvent<HTMLFormElement>) => {
+  const handleAddition = async (
+    e: FormEvent<HTMLFormElement>,
+    formData: FormData,
+    setFormData: Dispatch<SetStateAction<FormData>>
+  ) => {
     e.preventDefault();
     try {
       const { name, email, newComment } = formData;
@@ -80,7 +62,6 @@ const PostDetail = () => {
         newComment
       );
       setFormData({ name: "", email: "", newComment: "" });
-
       resNewComment &&
         setComments((prevComments) => [resNewComment, ...prevComments]);
     } catch (error) {
@@ -110,128 +91,45 @@ const PostDetail = () => {
 
   return (
     <div className="h-auto">
-      {comments.length ? (
+      <div
+        className="flex flex-col items-center"
+        style={{ position: "relative", top: "-30px" }}
+      >
+        {openModal && (
+          <DeletionModal
+            handleDeletion={() => handleDeletion(currentCommentId as number)}
+            setOpenModal={setOpenModal}
+          />
+        )}
+        {post && <Post post={post} />}
         <div
-          className=" flex flex-col items-center"
-          style={{ position: "relative", top: "-30px" }}
+          id="input-comments"
+          className="flex items-center flex-col w-[1000px] border rounded-md"
         >
-          {openModal && (
-            <DeletionModal
-              handleDeletion={() => handleDeletion(currentCommentId as number)}
-              setOpenModal={setOpenModal}
-            />
-          )}
-          {post && <Post post={post} />}
-          <div
-            id="input-comments"
-            className="flex items-center flex-col w-[1000px] border rounded-md"
-          >
-            <div className="flex my-5 mx-5 w-full">
-              <form onSubmit={handleAddition} className="w-full px-10">
-                <div className="flex w-7/12 mb-5">
-                  <input
-                    type="text"
-                    className="w-full my-1 border rounded-md min-h-8 pl-4"
-                    id="name-input"
-                    name="name"
-                    placeholder="Name..."
-                    value={formData.name}
-                    onChange={handleFormChange}
-                  />
-                  <input
-                    type="email"
-                    className="w-full my-1 border rounded-md ml-5 min-h-8 pl-4"
-                    id="email-input"
-                    name="email"
-                    placeholder="Email..."
-                    value={formData.email}
-                    onChange={handleFormChange}
-                  />
-                </div>
-                <textarea
-                  value={formData.newComment}
-                  name="newComment"
-                  placeholder="Leave a comment..."
-                  onChange={handleFormChange}
-                  rows={3}
-                  className="w-full p-2 border rounded-md"
-                />
-                <div id="add-btn-container" className="flex justify-end">
-                  <button
-                    className="min-w-[120px] bg-red-500 text-white rounded-md text-sm min-h-6"
-                    type="submit"
-                  >
-                    Add comment
-                  </button>
-                </div>
-              </form>
-            </div>
-            <div>
-              {currentItems.map((comment) => (
-                <div
-                  key={comment.id}
-                  className="border-l-2 border-grey hover:border-bi-red m-2.5 p-2.5"
-                >
-                  <div className="flex justify-between">
-                    <div>
-                      <p className="font-medium">{comment.email}</p>
-                      <p className="text-sm">{comment.name}</p>
-                    </div>
-                    <div className="flex gap-4">
-                      <button
-                        onClick={() => handleEditing(comment.id, comment.body)}
-                      >
-                        <RiEdit2Line color={"#505050"} size={"1.3em"} />
-                      </button>
-                      <button
-                        onClick={() =>
-                          !editingCommentId && handleModal(comment.id)
-                        }
-                      >
-                        <RiDeleteBin2Line color={"#505050"} size={"1.3em"} />
-                      </button>
-                    </div>
-                  </div>
-
-                  {editingCommentId === comment.id ? (
-                    <div>
-                      <textarea
-                        value={editedText}
-                        onChange={(e) => setEditedText(e.target.value)}
-                        autoFocus
-                        rows={3}
-                        className="w-full p-2 border border-gray-300 text-sm"
-                      />
-                      <button
-                        className="min-w-[100px] bg-green-500 text-white rounded-md text-sm min-h-6 mr-4"
-                        onClick={() => handleSave(comment.id)}
-                      >
-                        Confirms
-                      </button>
-                      <button
-                        className="min-w-[100px] bg-red-500 text-white rounded-md text-sm min-h-6"
-                        onClick={() => setEditingCommentId(null)}
-                      >
-                        Cancel
-                      </button>
-                    </div>
-                  ) : (
-                    <p className="text-xs">{comment.body}</p>
-                  )}
-                </div>
-              ))}
+          <div className="flex my-5 mx-5 w-full">
+            <Form handleAddition={handleAddition} />
+          </div>
+          {comments.length ? (
+            <>
+              <CommentsList
+                comments={currentItems}
+                handleEditing={handleEditing}
+                editingCommentId={editingCommentId}
+                handleSave={handleSave}
+                handleModal={handleModal}
+              />
               <Pagination
                 length={comments.length}
                 postsPerPage={itemsPerPage}
                 handlePagination={handlePagination}
                 currentPage={currentPage}
               />
-            </div>
-          </div>
+            </>
+          ) : (
+            <div className="my-4 text-sm">No comments yet. Add a new one!</div>
+          )}
         </div>
-      ) : (
-        <div>No comments yet. Add a new one!</div>
-      )}
+      </div>
     </div>
   );
 };
